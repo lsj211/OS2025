39499e8392a105d6bc8309328da24b3ed6800634
Zhang Ziheng <m17333499304@163.com>
2026-01-02 15:19:58 +0800

    update s

--- lab6/kern/schedule/default_sched_stride.c
+++ lab6/kern/schedule/default_sched_stride.c
@@ -9,7 +9,7 @@
 
 /* You should define the BigStride constant here*/
 /* LAB6 CHALLENGE 1: YOUR CODE */
-#define BIG_STRIDE /* you should give a value, and is ??? */
+#define BIG_STRIDE 0x7FFFFFFF
 
 /* The compare function for two skew_heap_node_t's and the
  * corresponding procs*/
@@ -46,6 +46,9 @@ stride_init(struct run_queue *rq)
       * (2) init the run pool: rq->lab6_run_pool
       * (3) set number of process: rq->proc_num to 0
       */
+     list_init(&(rq->run_list));
+     rq->lab6_run_pool = NULL;
+     rq->proc_num = 0;
 }
 
 /*
@@ -73,6 +76,22 @@ stride_enqueue(struct run_queue *rq, struct proc_struct *proc)
       * (3) set proc->rq pointer to rq
       * (4) increase rq->proc_num
       */
+     if (proc->time_slice == 0 || proc->time_slice > rq->max_time_slice)
+     {
+          proc->time_slice = rq->max_time_slice;
+     }
+
+     proc->rq = rq;
+     if (USE_SKEW_HEAP)
+     {
+          rq->lab6_run_pool = skew_heap_insert(rq->lab6_run_pool, &(proc->lab6_run_pool), proc_stride_comp_f);
+     }
+     else
+     {
+          assert(list_empty(&(proc->run_link)));
+          list_add_before(&(rq->run_list), &(proc->run_link));
+     }
+     rq->proc_num++;
 }
 
 /*
@@ -92,6 +111,16 @@ stride_dequeue(struct run_queue *rq, struct proc_struct *proc)
       *         skew_heap_remove: remove a entry from skew_heap
       *         list_del_init: remove a entry from the  list
       */
+     if (USE_SKEW_HEAP)
+     {
+          rq->lab6_run_pool = skew_heap_remove(rq->lab6_run_pool, &(proc->lab6_run_pool), proc_stride_comp_f);
+     }
+     else
+     {
+          assert(!list_empty(&(proc->run_link)));
+          list_del_init(&(proc->run_link));
+     }
+     rq->proc_num--;
 }
 /*
  * stride_pick_next pick the element from the ``run-queue'', with the
@@ -116,6 +145,43 @@ stride_pick_next(struct run_queue *rq)
       * (2) update p;s stride value: p->lab6_stride
       * (3) return p
       */
+     if (rq->proc_num == 0)
+     {
+          return NULL;
+     }
+
+     struct proc_struct *p = NULL;
+     if (USE_SKEW_HEAP)
+     {
+          if (rq->lab6_run_pool == NULL)
+          {
+               return NULL;
+          }
+          p = le2proc(rq->lab6_run_pool, lab6_run_pool);
+     }
+     else
+     {
+          list_entry_t *le = list_next(&(rq->run_list));
+          if (le == &(rq->run_list))
+          {
+               return NULL;
+          }
+          p = le2proc(le, run_link);
+          for (le = list_next(le); le != &(rq->run_list); le = list_next(le))
+          {
+               struct proc_struct *q = le2proc(le, run_link);
+               if (proc_stride_comp_f(&(p->lab6_run_pool), &(q->lab6_run_pool)) > 0)
+               {
+                    p = q;
+               }
+          }
+     }
+
+     if (p != NULL)
+     {
+          p->lab6_stride += BIG_STRIDE / p->lab6_priority;
+     }
+     return p;
 }
 
 /*
@@ -130,6 +196,14 @@ static void
 stride_proc_tick(struct run_queue *rq, struct proc_struct *proc)
 {
      /* LAB6 CHALLENGE 1: YOUR CODE */
+     if (proc->time_slice > 0)
+     {
+          proc->time_slice--;
+     }
+     if (proc->time_slice == 0)
+     {
+          proc->need_resched = 1;
+     }
 }
 
 struct sched_class stride_sched_class = {
